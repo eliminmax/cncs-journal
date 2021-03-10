dfcea7d1deab995d525b577085ca3b02a0986ed4
Eli Array Minkoff <e.array.minkoff@pm.me>
2021-03-10 17:10:13 -0500

    Fix bugs in secure-ssh.sh

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -2,9 +2,14 @@
 # author eliminmax
 
 if [ "$EUID" -ne 0 ]; then
-	echo -e "/033[1;31mThis script must be run as root./033[0m"
+	echo "This script must be run as root."
 	exit 1
 fi
+if [ "$#" -ne 1 ]; then
+	echo -e "This script must take exactly 1 argument."
+	exit 2
+fi
+
 
 username=$1
 
@@ -17,12 +22,12 @@ mkdir -p /home/${username}/.ssh
 if [ -f ../public-keys/id_rsa.pub ]; then
 	cat ../public-keys/id_rsa.pub >> /home/${username}/.ssh/authorized_keys
 else
-	curl https://raw.githubusercontent.com/eliminmax/cncs_journal/master/SYS265/public-keys/id_rsa.pub >> /home/${username}/.ssh/authorized_keys || exit 2
+	curl https://raw.githubusercontent.com/eliminmax/cncs_journal/master/SYS265/public-keys/id_rsa.pub >> /home/${username}/.ssh/authorized_keys || exit 3
 fi
 
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config.backup
 sed -i 's/ *#? *PermitRootLogin.*/PermitRootLogin no/g' /etc/ssh/sshd_config
 
-chown -R /home/${username}/.ssh ${username}:
+chown -R ${username}: /home/${username}/.ssh
 chmod 700  /home/${username}/.ssh/authorized_keys
 
