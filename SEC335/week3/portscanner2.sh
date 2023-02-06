a8e6dd420511677f38b8994ae1745b5a02aaf2e8
Eli Array Minkoff <eli@earrayminkoff.tech>
2023-02-05 21:35:05 -0500

    Updated portscanner2.sh

--- SEC335/week3/portscanner2.sh
+++ SEC335/week3/portscanner2.sh
@@ -1,14 +1,28 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# A bunch of stuff copied and pasted from week 2's scanner.sh into last semester's portscanner2.sh.
+
+set -o pipefail
+set -o noclobber
+set -o errexit
+set -o noglob
+set +o nounset
+if [ -t 2 ] && [ -z "${NO_COLOR+x}" ]; then 
+  error_msg () { printf '\e[1;31mERROR: \e[22m%s\e[m\n' "$*" >&2 ; }
+else
+  error_msg () { printf 'ERROR: %s' "$*" >&2 ; }
+fi
+set -o nounset
 
 ## PARSE OPTIONS
 while [ $# -gt 2 ]; do
-	if [ "$1" = '--debug' ]; then
+	if [ "$1" = '--debug' ]; then # lazy debugging implementation that I use rather frequently in shell scripts
 		set -x
 		shift
 	fi
 done
 if [ $# -ne 2 ]; then
-	echo "Expected 2 arguments, got $#" >&2
+	error_msg "Expected 2 arguments, got $#." >&2
 	exit 1
 fi
 
@@ -19,6 +33,8 @@ port="$2"
 echo "ip,port"
 
 for ip in "$network".{1..254}; do
-	timeout .1 bash -c "echo >/dev/tcp/$ip/$port" 2>/dev/null && echo "$ip,$port"
+	if timeout .1 bash -c "echo >/dev/tcp/$ip/$port" 2>/dev/null; then
+	       	echo "$ip,$port"
+	fi
 done
-
+exit 0
